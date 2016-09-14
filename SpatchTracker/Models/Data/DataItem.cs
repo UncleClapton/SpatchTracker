909abb06a6b3c871a85475322262a8bf2269497a
UncleClapton <camjwelt@gmail.com>
2016-09-14 08:00:12 -0400

    Begin adding data models.

---
+++ SpatchTracker/Models/Data/DataItem.cs
@@ -0,0 +1,24 @@
+﻿using Livet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpatchTracker.Models.Data
+{
+    public class DataItem {
+        public string Key { get; set; }
+
+        public object Value { get; set; }
+
+        public DataItem()
+        { }
+
+        public DataItem(string key, object value)
+        {
+            Key = key.ToLower();
+            Value = value;
+        }
+    }
+}
---
+++ SpatchTracker/Models/Data/Rat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpatchTracker.Models.Data
+{
+    public class Rat
+    {
+        public string UUID { get; set; }
+        public string CmdrName { get; set; }
+        public List<DataItem> Data { get; set; }
+        public DateTime Joined { get; set; }
+        public Platform Platform { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Rat ()
+        {
+
+        }
+
+        public Rat (string newUUID, string newCmdrName, List<DataItem> data, DateTime joined, Platform platform, DateTime createdAt)
+        {
+
+        }
+
+
+    }
+}
