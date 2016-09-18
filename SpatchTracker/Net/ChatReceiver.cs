1ebe11fd21a9afcb2f35b59023db7de7c6dd3c8c
UncleClapton <camjwelt@gmail.com>
2016-09-18 14:36:56 -0400

    Revert "Some days, clapton is an idiot. Today is that day."
    This reverts commit adb83b8bee3af5a632bdd80e9b48a9bc1991935c.

--- SpatchTracker/Net/ChatReceiver.cs
+++ SpatchTracker/Net/ChatReceiver.cs
@@ -1,30 +1,72 @@
-﻿using SpatchTracker.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using uhttpsharp;
+using uhttpsharp.Listeners;
+using uhttpsharp.RequestProviders;
+using System.Net;
 using SpatchTracker.Services;
-using System;
 using System.Text.RegularExpressions;
+using SpatchTracker.Models;
 
 namespace SpatchTracker.Net
 {
     public class ChatReceiver
     {
+        public HttpServer ListenerServer { get; private set; }
+
         public ChatReceiver()
         {
-            try
+            using (ListenerServer = new HttpServer(new HttpRequestProvider()))
             {
-                LoggingService.Current.Log("Starting Chat Event Receiver.", LogType.Info, LogLevel.Verbose);
-            }
-            catch (Exception e)
-            {
-                LoggingService.Current.Log("Unable to start Chat Event Receiver. Possible port conflict!", LogType.Error, LogLevel.Error);
-                LoggingService.Current.Log($"Error while initilizing WS Listener.\n------ Start Stack Trace ------\n{e.Message}\n{e.StackTrace}\n------ End stack trace ------", LogType.Error, LogLevel.Verbose);
-                Clapton.Exceptions.ExceptionHandling.ReportException(this, e);
+                try
+                {
+                    LoggingService.Current.Log("Starting Chat Event Receiver.", LogType.Info, LogLevel.Verbose);
+                    ListenerServer.Use(new TcpListenerAdapter(new TcpListener(IPAddress.Loopback, Settings.Current.ReceiverPort)));
+                }
+                catch (Exception e)
+                {
+                    LoggingService.Current.Log("Unable to start Chat Event Receiver. Possible port conflict!", LogType.Error, LogLevel.Error);
+                    LoggingService.Current.Log($"Error while initilizing HTTP Server.\n------ Start Stack Trace ------\n{e.Message}\n{e.StackTrace}\n------ End stack trace ------", LogType.Error, LogLevel.Verbose);
+                    Clapton.Exceptions.ExceptionHandling.ReportException(this, e);
+                }
+
+                ListenerServer.Use((context, next) =>
+                {
+                    context.Response = HttpResponse.CreateWithMessage(HttpResponseCode.Ok, "Received", false);
+
+                    var messageType = GetQueryStringProperty(context.Request, "mt");
+
+                    switch (messageType)
+                    {
+                        case "RSIG":
+                            HandleIncomingRsignal(context);
+                            break;
+                        default:
+                            LoggingService.Current.Log($"ChatReceiver has recieved a message, is of invalid type. MT = {messageType}", LogType.Info, LogLevel.Info);
+                            break;
+                    }
+
+
+                    return Task.Factory.GetCompleted();
+                });
             }
         }
 
-        private void HandleIncomingRsignal()
+        private static string GetQueryStringProperty(IHttpRequest request, string queryStringName)
+        {
+            var propertyValue = "";
+            request.QueryString.TryGetByName(queryStringName, out propertyValue);
+            return WebUtility.UrlDecode(propertyValue);
+        }
+
+        private void HandleIncomingRsignal(IHttpContext context)
         {
             //EX: RATSIGNAL - CMDR A Client - System: SystemName - Platform: PC - O2: OK - Language: English (en-US) - IRC Nickname: A_Client (Case #1)
-            var message = "";
+            var message = GetQueryStringProperty(context.Request, "msg");
 
             string cmdr = Regex.Match(message, @"CMDR (.*?) -", RegexOptions.IgnoreCase).Value;
             string system = Regex.Match(message, @"System: (.*?) -", RegexOptions.IgnoreCase).Value;
