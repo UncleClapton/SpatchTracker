5b7f636e452ed9bee091d2628c02aa201fc9eb2b
UncleClapton <camjwelt@gmail.com>
2016-09-25 17:49:01 +0000

    Sort model properties.

--- SpatchTracker/Models/Rescue.cs
+++ SpatchTracker/Models/Rescue.cs
@@ -5,19 +5,20 @@ namespace SpatchTracker.Models
 {
     public class Rescue
     {
-        public int BoardID { get; set; } = 100;
-        public string ClientNick { get; set; } = "";
         public bool Active { get; set; } = true;
-        public string ClientName { get; set; } = "";
         public bool CodeRed { get; set; } = false;
-        public Platform Platform { get; set; } = Platform.PC;
-        public List<string> Quotes { get; set; } = new List<string>();
-        public string System { get; set; } = "";
-        public string Title { get; set; } = "";
+        public bool Epic { get; set; } = false;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public int BoardID { get; set; } = 100;
         public List<RescueRat> AssignedRats { get; set; } = new List<RescueRat>();
-        public bool Epic { get; set; } = false;
+        public List<string> Quotes { get; set; } = new List<string>();
+        public Platform Platform { get; set; } = Platform.PC;
+        public string ClientName { get; set; } = "";
+        public string ClientNick { get; set; } = "";
+        public string Language { get; set; } = "English (en-US)";
+        public string System { get; set; } = "";
+        public string Title { get; set; } = "";
 
         public Rescue () {}
     }
