71baa2c5f6f1cee493ac9718b95964663a52c9e5
UncleClapton <camjwelt@gmail.com>
2016-09-25 17:50:18 +0000

    Have HandleRatsignal make a new rescue and add the rescue to the board.

--- SpatchTracker/Net/MessageHandlers.cs
+++ SpatchTracker/Net/MessageHandlers.cs
@@ -1,6 +1,7 @@
 ﻿using Clapton.Extensions;
 using SpatchTracker.Models;
 using SpatchTracker.Services;
+using System;
 using System.Text.RegularExpressions;
 using uhttpsharp;
 
@@ -13,14 +14,20 @@ namespace SpatchTracker.Net
         {
             //EX: RATSIGNAL - CMDR A Client - System: SystemName - Platform: PC - O2: OK - Language: English (en-US) - IRC Nickname: A_Client (Case #1)
 
-            string cmdr = Regex.Match(message, @"CMDR (.+?) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "Unknown";
-            string system = Regex.Match(message, @"System: (.+?) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "Unknown";
-            Platform platform = (Regex.Match(message, @"Platform: (XB|PC) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "PC") == "XB" ? Platform.XB : Platform.PC;
-            bool codeRed = (Regex.Match(message, @"O2: ((?:NOT )?OK) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "OK") == "NOT OK" ? true : false;
-            string language = Regex.Match(message, @"Language: (.+?) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "Unknown";
-            string ircNick = Regex.Match(message, @"IRC Nickname: (.+?) \(", RegexOptions.IgnoreCase).Groups[1].Value ?? cmdr ?? "Unknown";
-            int? boardIndex = (Regex.Match(message, @"\(Case #(\d+?)\)", RegexOptions.IgnoreCase).Groups[1].Value ?? "0").ToNullableInt() ?? 0;
-            LoggingService.Current.Log($"Incoming Client: CMDR {cmdr} | System: {system} | Platform : {platform.ToString()} | CR: {codeRed.ToString()} | Lang: {language} | IRC: {ircNick} | Case #{boardIndex}", LogType.Incoming, LogLevel.Debug);
+            Rescue newRescue = new Rescue();
+
+            newRescue.ClientName = Regex.Match(message, @"CMDR (.+?) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "Client";
+            newRescue.System = Regex.Match(message, @"System: (.+?) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "N/A";
+            newRescue.Platform = (Regex.Match(message, @"Platform: (XB|PC) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "PC") == "XB" ? Platform.XB : Platform.PC;
+            newRescue.CodeRed = (Regex.Match(message, @"O2: ((?:NOT )?OK) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "OK") == "NOT OK" ? true : false;
+            newRescue.Language = Regex.Match(message, @"Language: (.+?) -", RegexOptions.IgnoreCase).Groups[1].Value ?? "English (en-US)";
+            newRescue.ClientNick = Regex.Match(message, @"IRC Nickname: (.+?) \(", RegexOptions.IgnoreCase).Groups[1].Value ?? newRescue.ClientName ?? "A_Client";
+            newRescue.BoardID = (Regex.Match(message, @"\(Case #(\d+?)\)", RegexOptions.IgnoreCase).Groups[1].Value ?? "0").ToNullableInt() ?? 0;
+            newRescue.CreatedAt = DateTime.Now;
+            newRescue.UpdatedAt = DateTime.Now;
+
+            LoggingService.Current.Log(nameof(MessageHandlers), $"Incoming Client: CMDR {newRescue.ClientName} | System: {newRescue.System} | Platform : {newRescue.Platform.ToString()} | CR: {newRescue.CodeRed.ToString()} | Lang: {newRescue.Language} | IRC: {newRescue.ClientNick} | Case #{newRescue.BoardID}", LogLevel.Verbose);
+            RatBoard.Current.AddRescue(newRescue);
         }
 
         [MessageType("assign")]
