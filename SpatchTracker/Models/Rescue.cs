e5110d6e333e04769164de77d9168e7eb2d517c6
UncleClapton <camjwelt@gmail.com>
2016-09-28 15:35:14 +0000

    expand Rescue into an Observable class. make all properties observable

--- SpatchTracker/Models/Rescue.cs
+++ SpatchTracker/Models/Rescue.cs
@@ -1,26 +1,221 @@
-﻿using System;
+﻿using Livet;
+using System;
 using System.Collections.Generic;
+using Clapton.Extensions;
+using Clapton.Collections;
 
 namespace SpatchTracker.Models
 {
-    public class Rescue
+    public class Rescue : NotificationObject
     {
-        public bool Active { get; set; } = true;
-        public bool CodeRed { get; set; } = false;
-        public bool Epic { get; set; } = false;
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
-        public int BoardID { get; set; } = 100;
-        //              Rat name, Rat info
-        public Dictionary<string, RescueRat> AssignedRats { get; set; } = new Dictionary<string, RescueRat>();
-        public List<string> Quotes { get; set; } = new List<string>();
-        public Platform Platform { get; set; } = Platform.PC;
-        public string ClientName { get; set; } = "";
-        public string ClientNick { get; set; } = "";
-        public string Language { get; set; } = "English (en-US)";
-        public string System { get; set; } = "";
-        public string Title { get; set; } = "";
+        private bool active;
+        public bool Active
+        {
+            get { return active; }
+            set
+            {
+                if (active != value)
+                {
+                    active = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
 
-        public Rescue () {}
+        private bool codeRed;
+        public bool CodeRed
+        {
+            get { return codeRed; }
+            set
+            {
+                if (codeRed != value)
+                {
+                    codeRed = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private bool epic;
+        public bool Epic
+        {
+            get { return epic; }
+            set
+            {
+                if (epic != value)
+                {
+                    epic = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private DateTime createdAt;
+        public DateTime CreatedAt
+        {
+            get { return createdAt; }
+            set
+            {
+                if (createdAt != value)
+                {
+                    createdAt = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private DateTime updatedAt;
+        public DateTime UpdatedAt
+        {
+            get { return updatedAt; }
+            set
+            {
+                if (updatedAt != value)
+                {
+                    updatedAt = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private int boardID;
+        public int BoardID
+        {
+            get { return boardID; }
+            set
+            {
+                if (boardID != value)
+                {
+                    boardID = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private ObservableDictionary<string, RescueRat> assignedRats;
+        public ObservableDictionary<string, RescueRat> AssignedRats
+        {
+            get { return assignedRats; }
+            set
+            {
+                if (assignedRats != value)
+                {
+                    assignedRats = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private List<string> quotes;
+        public List<string> Quotes
+        {
+            get { return quotes; }
+            set
+            {
+                if (quotes != value)
+                {
+                    quotes = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private Platform platform;
+        public Platform Platform
+        {
+            get { return platform; }
+            set
+            {
+                if (platform != value)
+                {
+                    platform = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string clientName;
+        public string ClientName
+        {
+            get { return clientName; }
+            set
+            {
+                if (clientName != value)
+                {
+                    clientName = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string clientNick;
+        public string ClientNick
+        {
+            get { return clientNick; }
+            set
+            {
+                if (clientNick != value)
+                {
+                    clientNick = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string language;
+        public string Language
+        {
+            get { return language; }
+            set
+            {
+                if (language != value)
+                {
+                    language = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string system;
+        public string System
+        {
+            get { return system; }
+            set
+            {
+                if (system != value)
+                {
+                    system = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        public Rescue ()
+        {
+            // Always update UpdatedAt when a property is changed, or when the collections have been updated.
+            this.Subscribe((sender, args) =>
+            {
+                if (args.PropertyName != nameof(UpdatedAt))
+                    UpdatedAt = DateTime.Now;
+            });
+            assignedRats.CollectionChanged += (sender, args) =>
+            {
+                UpdatedAt = DateTime.Now;
+            };
+        }
     }
 }
