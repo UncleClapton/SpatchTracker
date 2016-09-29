a381cf5d5877cc42c6a4ddade4ce759eb7387bd7
UncleClapton <camjwelt@gmail.com>
2016-09-29 13:38:27 -0400

    Add object extensions. I WANT TO TRIM DOWN CUL, NOT EXPAND IT!

---
+++ CUL/Extensions/ObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clapton.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Determines whether the given <see cref="object"/> matches any specified <see cref="object"/>.
+        /// </summary>
+        /// <param name="obj">The selected <see cref="object"/></param>
+        /// <param name="value">The <see cref="object"/> to compare to the given object</param>
+        /// <returns></returns>
+        public static bool Equals(this object obj, params object[] values)
+        {
+            foreach (string value in values)
+                if (obj.Equals(value)) return true;
+            return false;
+        }
+
+    }
+}
