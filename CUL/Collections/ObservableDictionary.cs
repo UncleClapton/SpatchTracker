5654e338d1a566a362886614e6997d7823bbb26d
UncleClapton <camjwelt@gmail.com>
2016-09-29 13:39:57 -0400

    Finish ObservableDictionary. Add credit

--- CUL/Collections/ObservableDictionary.cs
+++ CUL/Collections/ObservableDictionary.cs
@@ -1,26 +1,62 @@
-﻿using System;
+﻿// based on http://blogs.microsoft.co.il/shimmy/2010/12/26/observabledictionarylttkey-tvaluegt-c/ with modifications
+using Clapton.Extensions;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Collections;
+using System.Linq;
 
 namespace Clapton.Collections
 {
-    //TODO Finish Dictionary observation wrapper.
-#error ObservableDictionary<TKey, TValue> is not finished
-    public class ObservableDictionary<TKey, TValue> : IDictionary<TKey, TValue>, INotifyCollectionChanged, INotifyPropertyChanged , IReadOnlyDictionary<TKey, TValue>
+    public class ObservableDictionary<TKey, TValue> : IDictionary<TKey, TValue>, INotifyCollectionChanged, INotifyPropertyChanged
     {
         private IDictionary<TKey, TValue> _dictionary;
+
+        #region Constructors
+        public ObservableDictionary()
+        {
+            _dictionary = new Dictionary<TKey, TValue>();
+        }
+
+        public ObservableDictionary(IDictionary<TKey, TValue> dictionary)
+        {
+            _dictionary = new Dictionary<TKey, TValue>(dictionary);
+        }
+
+        public ObservableDictionary(IEqualityComparer<TKey> comparer)
+        {
+            _dictionary = new Dictionary<TKey, TValue>(comparer);
+        }
+
+        public ObservableDictionary(int capacity)
+        {
+            _dictionary = new Dictionary<TKey, TValue>(capacity);
+        }
+
+        public ObservableDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer)
+        {
+            _dictionary = new Dictionary<TKey, TValue>(dictionary, comparer);
+        }
+
+        public ObservableDictionary(int capacity, IEqualityComparer<TKey> comparer)
+        {
+            _dictionary = new Dictionary<TKey, TValue>(capacity, comparer);
+        }
+        #endregion
 
         public TValue this[TKey key]
         {
             get
             {
-                throw new NotImplementedException();
+                if (key == null) throw new ArgumentNullException(nameof(key));
+                return _dictionary[key];
             }
+
             set
             {
-                throw new NotImplementedException();
+                if (key == null) throw new ArgumentNullException(nameof(key));
+                Insert(key, value, false);
             }
         }
 
@@ -28,7 +64,7 @@ namespace Clapton.Collections
         {
             get
             {
-                throw new NotImplementedException();
+                return _dictionary.Count;
             }
         }
 
@@ -36,7 +72,7 @@ namespace Clapton.Collections
         {
             get
             {
-                throw new NotImplementedException();
+                return _dictionary.IsReadOnly;
             }
         }
 
@@ -44,7 +80,7 @@ namespace Clapton.Collections
         {
             get
             {
-                throw new NotImplementedException();
+                return _dictionary.Keys;
             }
         }
 
@@ -52,23 +88,7 @@ namespace Clapton.Collections
         {
             get
             {
-                throw new NotImplementedException();
-            }
-        }
-
-        IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values
-        {
-            get
-            {
-                throw new NotImplementedException();
+                return _dictionary.Values;
             }
         }
 
@@ -77,57 +97,116 @@ namespace Clapton.Collections
 
         public void Add(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            Insert(item.Key, item.Value);
         }
 
         public void Add(TKey key, TValue value)
         {
-            throw new NotImplementedException();
+            Insert(key, value);
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            if (Count == 0) return;
+            List<KeyValuePair<TKey, TValue>> items = _dictionary.ToList();
+            _dictionary.Clear();
+            if (Count == 0)
+                RaiseCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset, items));
+            items = null; //to ensure GC does it's work here.
         }
 
         public bool Contains(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            return _dictionary.Contains(item);
         }
 
         public bool ContainsKey(TKey key)
         {
-            throw new NotImplementedException();
+            return _dictionary.ContainsKey(key);
         }
 
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
+        {   
+            _dictionary.CopyTo(array, arrayIndex);
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotImplementedException();
+            return _dictionary.GetEnumerator();
         }
 
         public bool Remove(TKey key)
         {
-            throw new NotImplementedException();
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            TValue value;
+            if (_dictionary.TryGetValue(key, out value))
+            {
+                var removed = _dictionary.Remove(key);
+                if (removed)
+                    RaiseCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, new KeyValuePair<TKey, TValue>(key, value)));
+                return removed;
+            }
+            return false;
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return Remove(item.Key);
         }
 
         public bool TryGetValue(TKey key, out TValue value)
         {
-            throw new NotImplementedException();
+            return _dictionary.TryGetValue(key, out value);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return _dictionary.GetEnumerator();
+        }
+
+        private void Insert(TKey key, TValue value, bool add = true)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            TValue item;
+            if (_dictionary.TryGetValue(key, out item))
+            {
+                if (add)
+                    throw new ArgumentException("Key already exists.");
+                if (EqualityComparer<TValue>.Default.Equals(item, value))
+                    return;
+                _dictionary[key] = value;
+
+                RaiseCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, new KeyValuePair<TKey, TValue>(key, value), new KeyValuePair<TKey, TValue>(key, item)));
+            }
+            else
+            {
+                _dictionary[key] = value;
+
+                RaiseCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, new KeyValuePair<TKey, TValue>(key, value)));
+            }
+        }
+
+        private void RaiseCollectionChanged(NotifyCollectionChangedEventArgs args)
+        {
+            if (CollectionChanged != null)
+                CollectionChanged.Invoke(this, args);
+
+            RaisePropertyChanged(nameof(Values));
+
+            if (args.Action.Equals(NotifyCollectionChangedAction.Add, NotifyCollectionChangedAction.Remove, NotifyCollectionChangedAction.Reset))
+            {
+                RaisePropertyChanged(nameof(Keys));
+                RaisePropertyChanged(nameof(Count));
+            }
+
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
